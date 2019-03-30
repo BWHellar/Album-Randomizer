5ce089ce71327dbc7dce3a1fc2f7a086427cbc04
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-03-30 11:35:23 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var imagesArray = ["img/cookie.jpg", "img/bacon.jpg", "img/butter.jpg", "img/toast.jpg", "img/cheese.jpg"];
+
+function displayImage(){
+    var num = Math.floor(Math.random() * 5);
+    document.canvas.src = imagesArray[num];
+}
+
+
+
+
+
+
 $(document).ready(function(){
     $(".input").keyup(function(){
           var val1 = +$(".value1").val();
