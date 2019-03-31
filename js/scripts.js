da03b2e0a7ab524394314211858ec6443d5275ba
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-03-30 19:34:18 -0700

    add a function where information about the album to the right an be added

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,73 @@
-var imagesArray = ["img/2Bears.jpg", "img/AidKit.jpg", "img/Avalanche.jpg", "img/Chainless.jpg", "img/ChromeSparks.jpg", "img/Computer.jpg", "img/Crystal.jpg", "img/DFA.jpg", "img/DillyDally.jpg", "img/Eagles.jpg", "img/Emancipator.jpg", "img/Emancipator2.jpg", "img/Foals.jpg", "img/FrontBottom.jpg", "img/MillionDead.jpg", "img/Moderat.jpg", "img/Nosajthing.jpg", "img/Offend.jpg", "img/Pixies.jpg", "img/Presets.jpg", "img/QOTSA.jpg", "img/Ratatat.jpg", "img/Regina.jpg", "img/Shlomo.jpg", "img/Silversun.jpg", "img/Sleater.jpg", "img/Tricky.jpg", "img/Wax.jpg", "img/WyeOak.jpg", "img/Yeasayer.jpg"];
+// var imagesArray = ["img/2Bears.jpg", "img/AidKit.jpg", "img/Avalanche.jpg", "img/Chainless.jpg", "img/ChromeSparks.jpg", "img/Computer.jpg", "img/Crystal.jpg", "img/DFA.jpg", "img/DillyDally.jpg", "img/Eagles.jpg", "img/Emancipator.jpg", "img/Emancipator2.jpg", "img/Foals.jpg", "img/FrontBottom.jpg", "img/MillionDead.jpg", "img/Moderat.jpg", "img/Nosajthing.jpg", "img/Offend.jpg", "img/Pixies.jpg", "img/Presets.jpg", "img/QOTSA.jpg", "img/Ratatat.jpg", "img/Regina.jpg", "img/Shlomo.jpg", "img/Silversun.jpg", "img/Sleater.jpg", "img/Tricky.jpg", "img/Wax.jpg", "img/WyeOak.jpg", "img/Yeasayer.jpg"];
+
 
 function displayImage(){
+    var imagesArray = ["img/2Bears.jpg", "img/AidKit.jpg", "img/Avalanche.jpg", "img/Chainless.jpg", "img/ChromeSparks.jpg", "img/Computer.jpg", "img/Crystal.jpg", "img/DFA.jpg", "img/DillyDally.jpg", "img/Eagles.jpg", "img/Emancipator.jpg", "img/Emancipator2.jpg", "img/Foals.jpg", "img/FrontBottom.jpg", "img/MillionDead.jpg", "img/Moderat.jpg", "img/Nosajthing.jpg", "img/Offend.jpg", "img/Pixies.jpg", "img/Presets.jpg", "img/QOTSA.jpg", "img/Ratatat.jpg", "img/Regina.jpg", "img/Shlomo.jpg", "img/Silversun.jpg", "img/Sleater.jpg", "img/Tricky.jpg", "img/Wax.jpg", "img/WyeOak.jpg", "img/Yeasayer.jpg"];
     var num = Math.floor(Math.random() * 30);
     document.canvas.src = imagesArray[num];
+      if (num == [0]){
+        alert("HELLO");
+      } else if (num == [1]){
+        alert("NONONO");
+      } else if (num == [2]){
+        alert("kokokokokok");
+      } else if (num == [3]){
+        alert("kokokokokok");
+      } else if (num == [4]){
+        alert("kokokokokok");
+      } else if (num == [5]){
+        alert("kokokokokok");
+      } else if (num == [6]){
+        alert("kokokokokok");
+      } else if (num == [7]){
+        alert("kokokokokok");
+      } else if (num == [8]){
+        alert("kokokokokok");
+      } else if (num == [9]){
+        alert("kokokokokok");
+      } else if (num == [10]){
+        alert("kokokokokok");
+      } else if (num == [11]){
+        alert("kokokokokok");
+      } else if (num == [12]){
+        alert("kokokokokok");
+      } else if (num == [13]){
+        alert("kokokokokok");
+      } else if (num == [14]){
+        alert("kokokokokok");
+      } else if (num == [15]){
+        alert("kokokokokok");
+      } else if (num == [16]){
+        alert("kokokokokok");
+      } else if (num == [17]){
+        alert("kokokokokok");
+      } else if (num == [18]){
+        alert("kokokokokok");
+      } else if (num == [19]){
+        alert("kokokokokok");
+      } else if (num == [20]){
+        alert("kokokokokok");
+      } else if (num == [21]){
+        alert("kokokokokok");
+      } else if (num == [22]){
+        alert("kokokokokok");
+      } else if (num == [23]){
+        alert("kokokokokok");
+      } else if (num == [24]){
+        alert("kokokokokok");
+      } else if (num == [25]){
+        alert("kokokokokok");
+      } else if (num == [26]){
+        alert("kokokokokok");
+      } else if (num == [27]){
+        alert("kokokokokok");
+      } else if (num == [28]){
+        alert("kokokokokok");
+      } else if (num == [29]){
+        alert("kokokokokok");
+      } 
 }
 
 
-
-
-
-
 $(document).ready(function(){
-    $(".input").keyup(function(){
-          var val1 = +$(".value1").val();
-          var val2 = +$(".value2").val();
-          $("#result").val(val1+val2);
-    });
 });
